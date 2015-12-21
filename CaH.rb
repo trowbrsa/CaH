a78af689bb9ca31d1544f0a99640e7a709770da8
Meighan Rasley <meighanr@gmail.com>
2015-12-21 15:28:44 -0800

    Lists players

--- CaH.rb
+++ CaH.rb
@@ -13,6 +13,17 @@ module Game
 
     def get_players
       puts "Let's play Cards Against Humanity!"
+      puts "How many players do you have?"
+      num_players = gets.chomp.to_i
+      count = 0
+      players = []
+      while count < num_players
+        puts "Please enter a player's name:"
+        player_name = gets.chomp
+        count += 1
+        players.push(player_name)
+      end
+      puts players
     end
 
     def play
