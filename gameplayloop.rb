43ca7cb70bb48da8b45c61ef57a71e85265f0bd6
Meighan Rasley <meighanr@gmail.com>
2015-12-21 14:56:24 -0800

    Move board logic to gameplayloop

--- gameplayloop.rb
+++ gameplayloop.rb
@@ -1,5 +1,18 @@
 module Game
   class GamePlayLoop
+    puts "Let's play Cards Against Humanity!"
+    puts "How many players do you have?"
+    num_players = gets.chomp.to_i
+    count = 0
+    players = []
+    while count < num_players
+      puts "Please enter a player's name:"
+      player_name = gets.chomp
+      count += 1
+      players.push(player_name)
+    # else
+      # call czar method and tell the users who the czar is
+    end
 
   end
 end
