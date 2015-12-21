9da369042ff4c8e4b55142349a0a23ef8454796f
Lauren Granger <lr.granger21@gmail.com>
2015-12-21 15:11:23 -0800

    moved cards class contents into board and set some bord initialize methods

--- board.rb
+++ board.rb
@@ -1,18 +1,29 @@
 module Game
-  class Cards
+  class Board
+    #initialize a board for a Game
     def initialize
-      make_white_deck
-      make_black_deck
+      # make a new array of all the players
+      players = #an array of players?
+      # set the white deck to be new
+      white_deck = make_white_deck
+      # set the black deck to be new
+      black_deck = make_black_deck
     end
+
     def make_white_deck
+      # read the whitecards csv file into and array
       white_deck = CSV.read("./csv/whitecards.csv")
     end
     def make_black_deck
+      # read the blackcards csv file into and array
       black_deck = CSV.read("./csv/blackcards.csv")
     end
 
-  class Board
+    def deal_cards
+      # deal one black card to the czar
+      # deal white cards to everyone so their hand is at 10
 
+    end
 
   end
 end
